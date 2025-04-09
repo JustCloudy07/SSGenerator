7e7736ae62ccbea3d0a4f95dba1a906dff470d7d
JustCloudy07 <gilbertgermo111@gmail.com>
2025-04-09 22:28:08 +0100

    changed root path

--- src/copy_static.py
+++ src/copy_static.py
@@ -36,7 +36,7 @@ def extract_title(markdown):
     raise Exception("Title not found")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(Path(__file__).parent.parent / from_path) as f:
         markdown_content = f.read()
@@ -48,7 +48,8 @@ def generate_page(from_path, template_path, dest_path):
     page_title = extract_title(markdown_content)
     template_content = template_content.replace("{{ Title }}", page_title)
     template_content = template_content.replace("{{ Content }}", html_string.to_html())
-
+    template_content = template_content.replace('href="/', f'href="{base}')
+    template_content = template_content.replace('src="/', f'src="{base}')
     if (
         os.path.exists(Path(__file__).parent.parent / os.path.dirname(dest_path))
         == False
@@ -60,17 +61,19 @@ def generate_page(from_path, template_path, dest_path):
         f.close()
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base):
     for item in os.listdir(Path(__file__).parent.parent / dir_path_content):
         if os.path.isfile(Path(__file__).parent.parent / dir_path_content / item):
             generate_page(
                 f"{dir_path_content}/{item}",
                 template_path,
                 f"{dest_dir_path}/{item}",
+                base,
             )
         else:
             generate_pages_recursive(
                 f"{dir_path_content}/{item}",
                 template_path,
                 f"{dest_dir_path}/{item}",
+                base,
             )
--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from copy_static import Copy_Static, generate_page, generate_pages_recursive
 from pathlib import Path
+import sys
 
 
 def main():
+    if sys.argv[0] != None:
+        basepath = sys.argv[0]
+    else:
+        basepath = "/"
     Copy_Static()
-    generate_pages_recursive("content", "template.html", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 main()
